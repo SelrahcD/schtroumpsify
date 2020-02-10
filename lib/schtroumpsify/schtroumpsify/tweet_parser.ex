e1e72d5ba50f3164a607e27d964fb9ade728a1d9
SelrahcD <c.desneuf@gmail.com>
2020-02-10 22:55:10 +0100

    Higher timeout for frmg request

--- lib/schtroumpsify/schtroumpsify/tweet_parser.ex
+++ lib/schtroumpsify/schtroumpsify/tweet_parser.ex
@@ -12,7 +12,7 @@ defmodule Schtroumpsify.TweetParser do
     preparedSentence = tweet.text
     |> String.replace("|", "")
 
-    response = HTTPoison.post!(Application.fetch_env!(:schtroumpsify, :frmg)[:url], {:form, [sentence: preparedSentence]})
+    response = HTTPoison.post!(Application.fetch_env!(:schtroumpsify, :frmg)[:url], {:form, [sentence: preparedSentence]}, [recv_timeout: 10000])
 
     Logger.debug("Parsing result #{tweet.id} #{response.body}")
 
