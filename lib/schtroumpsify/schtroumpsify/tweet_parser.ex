6b8019e1fd9e7af460948fe901cf729ba9534cfc
SelrahcD <c.desneuf@gmail.com>
2020-02-10 23:45:02 +0100

    Better log, stop after 30 secondes

--- lib/schtroumpsify/schtroumpsify/tweet_parser.ex
+++ lib/schtroumpsify/schtroumpsify/tweet_parser.ex
@@ -11,6 +11,10 @@ defmodule Schtroumpsify.TweetParser do
 
     preparedSentence = tweet.text
     |> String.replace("|", "")
+    |> String.replace("~r/https?:\/\/[^\s]+/", "")
+
+    Logger.info("Prepared sentend #{tweet.id} #{preparedSentence}")
+
 
     response = HTTPoison.post!(Application.fetch_env!(:schtroumpsify, :frmg)[:url], {:form, [sentence: preparedSentence]}, [recv_timeout: 30000])
 
