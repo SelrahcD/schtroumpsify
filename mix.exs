990550089ed4dce19f7700dc25c8e3a84762308e
SelrahcD <c.desneuf@gmail.com>
2021-07-04 18:28:46 +0200

    Faster CI

--- mix.exs
+++ mix.exs
@@ -60,4 +60,12 @@ defmodule Schtroumpsify.MixProject do
     [
     ]
   end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
+  end
+
 end
